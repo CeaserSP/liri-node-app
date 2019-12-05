03beb3b659df6244a329610ab55029b0a2b150d0
Sheldon Perkins <csperkins18@gmail.com>
2019-12-04 22:42:17 -0500

    concert URL, make request with axios to retrieve URL

--- liri.js
+++ liri.js
@@ -7,3 +7,12 @@ var moment = require("moment");
 var fs = require("fs");
 
 var spotify = new Spotify(keys.spotify);
+
+// concert-this Command
+function concert(artist){
+    var bQueryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+    // retrieve URL
+    axios.get(bQueryUrl).then(
+    
+    )
+}
