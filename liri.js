1d0ee32818389a82f0b4fd83f1b91829e0952c0d
Sheldon Perkins <csperkins18@gmail.com>
2019-12-04 23:01:50 -0500

    Venue and Location requests

--- liri.js
+++ liri.js
@@ -13,6 +13,11 @@ function concert(artist){
     var bQueryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
     // retrieve URL
     axios.get(bQueryUrl).then(
-    
+    function (response){
+        console.log("Artist: " + artist + 
+        " | " + "Venue: " + response.data[0].venue.name + 
+        " ( " + "Location: " + response.data[0].venue.country.region.city + ", " + response.data[0].venue.country.region + " | " + response.data[0].venue.country + " )" +
+        )
+    }
     )
 }
