7971782f144d4078a2572e884a306035d26e42a6
Sheldon Perkins <csperkins18@gmail.com>
2019-12-09 17:37:23 -0500

    default song

--- liri.js
+++ liri.js
@@ -9,22 +9,24 @@ var fs = require("fs");
 var res = process.argv[3]
 
 var spotify = new Spotify(keys.spotify);
-var omdb =
 
-    // concert-this Function
-    function concertThis(artist) {
-        var bQueryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
-        // retrieve URL
-        axios.get(bQueryUrl).then(
-            function (response) {
-                console.log("Artist: " + artist +
-                    " | " + "Venue: " + response.data[0].venue.name +
-                    " ( " + "Location: " + response.data[0].venue.country.region.city + ", " + response.data[0].venue.country.region + " | " + response.data[0].venue.country + " )" +
-                    " | " + "Date: " + moment().format(response.data[0].datetime));
-            })
-    }
+// concert-this Function
+function concertThis(artist) {
+    var bQueryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+    // retrieve URL
+    axios.get(bQueryUrl).then(
+        function (response) {
+            console.log("Artist: " + artist +
+                " | " + "Venue: " + response.data[0].venue.name +
+                " ( " + "Location: " + response.data[0].venue.country.region.city + ", " + response.data[0].venue.country.region + " | " + response.data[0].venue.country + " )" +
+                " | " + "Date: " + moment().format(response.data[0].datetime));
+        })
+}
 // spotify-this-song Function
 function spotifyThis(song) {
+    if (song === ''){
+        song = "The Sign";
+    }
     spotify.search({ type: "Song: ", res: song }, function (err, data) {
         if (err) {
             return ("Oops something went wrong... " + err)
@@ -46,14 +48,14 @@ function movieThis(movie) {
             if (!movie) {
                 movie = "Mr. Nobody."
             }
-            console.log("Title: " + response.data[0].Title + 
-            " | " + "Year: " + response.data[0].Year +
-            " | " + "Rated: " + response.data[0].Rated +
-            " | " + "Rotten Tomato Score: " + response.data[0].Ratings[2].Value +
-            " | " + "Country: " + response.data[0].Country +
-            " | " + "Language: " + response.data[0].Language+
-            " | " + "Plot: " + response.data[0].Plot +
-            " | " + "Actors: " + response.data[0].Actors)
+            console.log("Title: " + response.data[0].Title +
+                " | " + "Year: " + response.data[0].Year +
+                " | " + "Rated: " + response.data[0].Rated +
+                " | " + "Rotten Tomato Score: " + response.data[0].Ratings[2].Value +
+                " | " + "Country: " + response.data[0].Country +
+                " | " + "Language: " + response.data[0].Language +
+                " | " + "Plot: " + response.data[0].Plot +
+                " | " + "Actors: " + response.data[0].Actors)
         }
     )
 }
