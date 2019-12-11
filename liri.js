43bd3381aafc68dbd2f5d789eeeda2932fa4f62d
Sheldon Perkins <csperkins18@gmail.com>
2019-12-11 00:53:24 -0500

    movieThis fix. spotifyThis fix

--- liri.js
+++ liri.js
@@ -12,28 +12,39 @@ var event = process.argv[2];
 // console.log("THis is an issue" +event)
 var spotify = new Spotify(keys.spotify);
 // switch statment to take in a three functions
-switch (event) {
-    case "spotify-this-song":
-        spotifyThis(res);
-        break;
-    case "concert-this":
-        concertThis(res);
-        break;
-    case "movie-this":
-        movieThis(res);
-        break;
-// fs read. retrieve parse
-default:
-    fs.readFile("random.txt", "utf8", function(error, data){
+function doIt(event, res) {
+    switch (event) {
+        case "spotify-this-song":
+            // if (res === undefined){
+
+            // }
+            spotifyThis(res);
+            break;
+        case "concert-this":
+            concertThis(res);
+            break;
+        case "movie-this":
+            movieThis(res);
+            break;
+        case "do-what-it-says":
+            doThis();
+            break;
+
+        default:
+            console.log("Invalid Command");
+    }
+}// fs read. retrieve parse
+function doThis() {
+    fs.readFile("random.txt", "utf8", function (error, data) {
         var data = data.split(",");
         var songDef = data[1];
-        if(error){
+        console.log(songDef);
+        if (error) {
             return console.log(error)
         }
         spotifyThis(songDef);
     })
 }
-
 // concert-this Function
 function concertThis(artist) {
     var bQueryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
@@ -50,15 +61,17 @@ function concertThis(artist) {
 function spotifyThis(song) {
     if (song === '') {
         song = "The Sign";
+        // console.log(song);
     }
-    spotify.search({ type: "track", query: song}, function (err, data) {
+    spotify.search({ type: 'track', query: song }, function (err, data) {
         if (err) {
             return console.log("Oops something went wrong... " + err);
         }
-        console.log("Artist: " + data.tracks.items[0].artists[0].name +
-            " | " + "Song: " + data.tracks.items[0].name +
-            " | " + "Preview: " + data.tracks.items[0].external_urls.spotify +
-            " | " + "Album: " + data.tracks.items[0].album.name)
+        var song = data.tracks.items[0];
+        console.log("Artist: " + song.artists[0].name +
+            " | " + "Song: " + song.name +
+            " | " + "Preview: " + song.external_urls.spotify +
+            " | " + "Album: " + song.album.name)
     })
 }
 // movie-this Function
@@ -66,20 +79,25 @@ function movieThis(movie) {
     if (!movie) {
         movie = "Mr. Nobody."
     }
+    // console.log(movie);
     var mQueryUrl = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
-    axios.get(bQueryUrl).then(
+    console.log(mQueryUrl);
+    axios.get(mQueryUrl).then(
         function (response) {
             if (!movie) {
                 movie = "Mr. Nobody."
             }
-            console.log("Title: " + response.data[0].Title +
-                " | " + "Year: " + response.data[0].Year +
-                " | " + "Rated: " + response.data[0].Rated +
-                " | " + "Rotten Tomato Score: " + response.data[0].Ratings[2].Value +
-                " | " + "Country: " + response.data[0].Country +
-                " | " + "Language: " + response.data[0].Language +
-                " | " + "Plot: " + response.data[0].Plot +
-                " | " + "Actors: " + response.data[0].Actors)
+            console.log("Title: " + response.data.Title +
+                " | " + "Year: " + response.data.Year +
+                " | " + "Rated: " + response.data.Rated +
+                " | " + "Rotten Tomato Score: " + response.data.Ratings[2].Value +
+                " | " + "Country: " + response.data.Country +
+                " | " + "Language: " + response.data.Language +
+                " | " + "Plot: " + response.data.Plot +
+                " | " + "Actors: " + response.data.Actors)
+            
         }
     )
 }
+
+doIt(event, res); 
